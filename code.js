999c42f609926244e2a3ffc1810695f1ce0441f0
Herr_Alien <alexandru.garofide@gmail.com>
2020-08-25 20:31:03 +0300

    added code

--- code.js
+++ code.js
@@ -113,5 +113,45 @@ var AppSettings = {
 AppLogic.init();
 AppSettings.init();
 
+
+function handleClientLoad() {
+        gapi.load('client:auth2', initClient);
+}
+
+function initClient() {
+
+      var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+      var SCOPES = "https://www.googleapis.com/auth/spreadsheets";
+
+
+        gapi.client.init({
+          apiKey: AppSettings.data.apiKey,
+          clientId: AppSettings.data.clientId,
+          discoveryDocs: DISCOVERY_DOCS,
+          scope: SCOPES
+        }).then(function () {
+          // Listen for sign-in state changes.
+          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+          // Handle the initial sign-in state.
+          updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        }, function(error) {
+          console.log(error);
+        });
+      }
+
+
+      function updateSigninStatus(isSignedIn) {
+        if (isSignedIn) {
+           gotoUrl("#home");
+        } 
+      }
+
+
+gotoUrl("#login");
+handleClientLoad();
+
 })();
+
+
 
